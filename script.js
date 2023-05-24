1bdf3290e5016b6c4f21b2362cc37259f3a43843
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-05-24 11:51:29 +0400

    add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let massiveNumbers = [1,2,3,4,5]
+console.log(massiveNumbers[0] + massiveNumbers[1] + massiveNumbers[2] + massiveNumbers[3] + massiveNumbers[4] );
+
+let massive = [
+    Name = " dimitri ",
+    Age = 19,
+    address = " kiacheli street",
+]
+const firstIndex = 0;
+const secondIndex = 1;
+const thirdIndex = 2;
+console.log(` my name is ${massive[firstIndex]}. i'm  ${massive[secondIndex]} years old,  and my address is ${massive[thirdIndex]}.`);
+
+if(massive[secondIndex] >=18) {
+    console.log("i'm an adult");
+}else (
+    console.log("i'm teenager ")
+)
+
+let massiveNumbers2 = [ 10 ,20 ,30 ,40 ,50] 
+    console.log(massiveNumbers2);
